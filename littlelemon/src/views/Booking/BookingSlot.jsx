1e94569f6516b9cd677ff487443e908ce6c0f8e3
kirubel <kirato747@gmail.com>
2024-01-04 09:47:12 +0300

    Fix error in List

--- littlelemon/src/views/Booking/BookingSlot.jsx
+++ littlelemon/src/views/Booking/BookingSlot.jsx
@@ -5,26 +5,32 @@ const BookingSlot = ({availableTimes, bookedTimes}) => {
   return (
     <div>
       <div className="items-start justify-center w-full md:w-full boder boder-red-900 flex my-8 md:ml-8">
-        <List
-          className="pr-10"
-          header={<div>Available Times</div>}
-          dataSource={availableTimes}
-          renderItem={(time) => (
-            <List.Item>
-              <p>{time}</p>
-            </List.Item>
-          )}
-        />
+        {availableTimes && (
+          <List
+            rowKey={(time) => time}
+            className="pr-10"
+            header={<div>Available Times</div>}
+            dataSource={availableTimes}
+            renderItem={(time) => (
+              <List.Item>
+                <p key={time}>{time}</p>
+              </List.Item>
+            )}
+          />
+        )}
 
-        <List
-          header={<div>Booked Times</div>}
-          dataSource={bookedTimes}
-          renderItem={(time) => (
-            <List.Item>
-              <p>{time}</p>
-            </List.Item>
-          )}
-        />
+        {bookedTimes && (
+          <List
+            rowKey={(time) => time}
+            header={<div>Booked Times</div>}
+            dataSource={bookedTimes}
+            renderItem={(time) => (
+              <List.Item>
+                <p key={time}>{time}</p>
+              </List.Item>
+            )}
+          />
+        )}
       </div>
     </div>
   );
